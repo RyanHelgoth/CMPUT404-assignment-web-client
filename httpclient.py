3d39710d6961213bacce3df2ae0978a70c047d7e
RyanHelgoth <ryan.helgoth@gmail.com>
2022-02-03 23:19:18 -0500

    Attempt at GET, still lots of bugs

--- httpclient.py
+++ httpclient.py
@@ -22,7 +22,7 @@ import sys
 import socket
 import re
 # you may use urllib to encode data appropriately
-import urllib.parse
+import urllib.parse as parse
 
 def help():
     print("httpclient.py [GET/POST] [URL]\n")
@@ -49,9 +49,7 @@ class HTTPClient(object):
         # Else, append string to list when \r\n is reached.
             
 
-        headers = []
-        for char in data:
-            pass
+        
 
 
         return None
@@ -60,7 +58,7 @@ class HTTPClient(object):
         return None
     
     def sendall(self, data):
-        self.socket.sendall(data.encode('utf-8'))
+        self.socket.sendall(data.encode('latin-1'))
         
     def close(self):
         self.socket.close()
@@ -75,24 +73,44 @@ class HTTPClient(object):
                 buffer.extend(part)
             else:
                 done = not part
-        return buffer.decode('utf-8')
+        return buffer.decode('latin-1')
 
     def GET(self, url, args=None):
-        request = "GET / HTTP/1.1\r\nHost: {}\r\n\r\n".format(url)
-        httpsPort = "443"
+        #url = http://www.cs.ualberta.ca/
+
+        urlParts = parse.urlparse(url) 
+        hostName = urlParts[1]  #hostName = www.cs.ualberta.ca
+        print(urlParts)
+
+        #ip = socket.gethostbyname(hostName)
+        request = "GET / HTTP/1.1\r\nHost: {}\r\n\r\n".format(hostName)
+        #httpsPort = 443
+        httpPort = 80 #Only need to handle http
         
-        self.connect(url, httpsPort)
+        self.connect(hostName, httpPort)
         self.sendall(request)
+        self.socket.shutdown(socket.SHUT_WR)
         response = self.recvall(self.socket)
         self.close()
 
-        code = self.get_code(response)
-        body = self.get_body(response)
-        headers = self.get_headers(response)
+        #code = self.get_code(response)
+        #body = self.get_body(response)
+        #headers = self.get_headers(response)
 
-        print(body)
+        responsePartition = response.partition("\r\n\r\n")
+        headers = responsePartition[0] + responsePartition[1]
+        body = responsePartition[2]
+        code = headers.partition("\r\n")[0].split(" ")[1]
         
         
+    
+        #body = response.split("\r\n") #TODO remove when done testing
+        #print("CODE: " + code)
+        #print("HEADERS:\n" + headers)
+        #print("BODY:\n" + body)
+        
+        print(response)
+
         return HTTPResponse(code, body)
 
     def POST(self, url, args=None):
@@ -105,6 +123,10 @@ class HTTPClient(object):
             return self.POST( url, args )
         else:
             return self.GET( url, args )
+
+    def cleanUrl(self, url):
+        #Return url and https boolean
+        return
     
 if __name__ == "__main__":
     client = HTTPClient()
@@ -122,3 +144,113 @@ if __name__ == "__main__":
     else:
         #TODO print response
         print(client.command( sys.argv[1] ))
+
+
+#Test run
+
+'''
+python3 freetests.py
+HTTP UP!
+
+E/usr/lib/python3.6/unittest/case.py:633: ResourceWarning: unclosed <socket.socket fd=4, family=AddressFamily.AF_INET, type=SocketKind.SOCK_STREAM, proto=0, laddr=('0.0.0.0', 0)>
+  outcome.errors.clear()
+FEEAn Exception was thrown for http://www.cs.ualberta.ca/
+FSending POST!
+FFHTTP Shutdown in tearDown
+
+HTTP has been shutdown!
+
+
+======================================================================
+ERROR: test404GET (__main__.TestHTTPClient)
+Test against 404 errors
+----------------------------------------------------------------------
+Traceback (most recent call last):
+  File "freetests.py", line 162, in test404GET
+    req = http.GET("http://%s:%d/49872398432" % (BASEHOST,BASEPORT) )
+  File "/home/student/Assignments/CMPUT404-assignment-web-client/httpclient.py", line 83, in GET
+    self.connect(url, httpPort)
+  File "/home/student/Assignments/CMPUT404-assignment-web-client/httpclient.py", line 40, in connect
+    self.socket.connect((host, port))
+socket.gaierror: [Errno -2] Name or service not known
+
+======================================================================
+ERROR: testGET (__main__.TestHTTPClient)
+Test HTTP GET
+----------------------------------------------------------------------
+Traceback (most recent call last):
+  File "freetests.py", line 180, in testGET
+    req = http.GET( url )
+  File "/home/student/Assignments/CMPUT404-assignment-web-client/httpclient.py", line 83, in GET
+    self.connect(url, httpPort)
+  File "/home/student/Assignments/CMPUT404-assignment-web-client/httpclient.py", line 40, in connect
+    self.socket.connect((host, port))
+socket.gaierror: [Errno -2] Name or service not known
+
+======================================================================
+ERROR: testGETHeaders (__main__.TestHTTPClient)
+Test HTTP GET Headers
+----------------------------------------------------------------------
+Traceback (most recent call last):
+  File "freetests.py", line 192, in testGETHeaders
+    req = http.GET( url )
+  File "/home/student/Assignments/CMPUT404-assignment-web-client/httpclient.py", line 83, in GET
+    self.connect(url, httpPort)
+  File "/home/student/Assignments/CMPUT404-assignment-web-client/httpclient.py", line 40, in connect
+    self.socket.connect((host, port))
+socket.gaierror: [Errno -2] Name or service not known
+
+======================================================================
+FAIL: test404POST (__main__.TestHTTPClient)
+Test against 404 errors
+----------------------------------------------------------------------
+Traceback (most recent call last):
+  File "freetests.py", line 172, in test404POST
+    self.assertTrue(req.code == 404)
+AssertionError: False is not true
+
+======================================================================
+FAIL: testInternetGets (__main__.TestHTTPClient)
+Test HTTP Get in the wild, these webservers are far less
+----------------------------------------------------------------------
+Traceback (most recent call last):
+  File "freetests.py", line 223, in testInternetGets
+    req = http.GET( url )
+socket.gaierror: [Errno -2] Name or service not known
+
+During handling of the above exception, another exception occurred:
+
+Traceback (most recent call last):
+  File "freetests.py", line 226, in testInternetGets
+    self.assertTrue( False, "An Exception was thrown for %s %s" % (url,e))
+AssertionError: False is not true : An Exception was thrown for http://www.cs.ualberta.ca/ [Errno -2] Name or service not known
+
+======================================================================
+FAIL: testPOST (__main__.TestHTTPClient)
+Test HTTP POST with an echo server
+----------------------------------------------------------------------
+Traceback (most recent call last):
+  File "freetests.py", line 250, in testPOST
+    self.assertTrue(req.code == 200)
+AssertionError: False is not true
+
+======================================================================
+FAIL: testPOSTHeaders (__main__.TestHTTPClient)
+Test HTTP POST Headers
+----------------------------------------------------------------------
+Traceback (most recent call last):
+  File "freetests.py", line 205, in testPOSTHeaders
+    self.assertTrue(req.code == 200,"Code is %s but I wanted a 200 OK" % req.code)
+AssertionError: False is not true : Code is 500 but I wanted a 200 OK
+
+----------------------------------------------------------------------
+Ran 7 tests in 2.520s
+
+FAILED (failures=4, errors=3)
+
+
+
+
+
+
+'''
